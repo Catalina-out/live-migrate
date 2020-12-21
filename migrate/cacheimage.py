5b115bfab533419a26981ece8b0a68de2ab8eee9
Catalina_out <18395614477@163.com>
2020-12-21 20:07:05 +0800

    update recovert

--- migrate/cacheimage.py
+++ migrate/cacheimage.py
@@ -2,7 +2,8 @@
 # /usr/bin/env python
 import os
 import hashlib
-
+from oslo_concurrency import processutils
+from oslo_utils import imageutils
 # /os_instance/_base directory is existence
 path_base = "/os_instance/_base/"
 
@@ -28,8 +29,25 @@ def copy_instance_file(vip, instance_path, dest_path):
 
 # qemu-img info the image
 def qemu_img_info(path, image_id):
-    cmd = ('env', 'LC_ALL=C', 'LANG=C', 'qemu-img', 'info', path, image_id)
-    os.system(cmd)
+    try:
+        cmd = ('env', 'LC_ALL=C', 'LANG=C', 'qemu-img', 'info', path, image_id)
+        out, err = processutils.execute(cmd)
+    except processutils.ProcessExecutionError as exp:
+        # this means we hit prlimits, make the exception more specific
+        if exp.exit_code == -9:
+            msg = (("qemu-img aborted by prlimits when inspecting "
+                    "%(path)s : %(exp)s") % {'path': path, 'exp': exp})
+        else:
+            msg = (("qemu-img failed to execute on %(path)s : %(exp)s") %
+                   {'path': path, 'exp': exp})
+        raise msg
+
+    if not out:
+        msg = (("Failed to run qemu-img info on %(path)s : %(error)s") %
+               {'path': path, 'error': err})
+        raise msg
+
+    return imageutils.QemuImgInfo(out)
 
 
 # convert qcow2 to raw
@@ -47,7 +65,10 @@ def fetch_to_raw(cluster_vip, image_id):
     os.rename(image_id, base_image_name)
     cmd = ('qemu-img', 'convert', '-f', 'qcow2', '-O', 'raw',  image_id, base_image_name)
     try:
-        os.system(cmd)
-    except Exception as exp:
-        print("convert image is failed:%s"%exp)
-    qemu_img_info(path_base, base_image_name)
+        processutils.execute(cmd)
+    except processutils.ProcessExecutionError as exp:
+        msg = (("Unable to convert image to %(format)s: %(exp)s") %{'format': image_id, 'exp': exp})
+        print(msg)
+    data = qemu_img_info(path_base, base_image_name)
+    if data.file_format != "raw":
+        raise (("Converted to raw, but format is now %s") %data.file_format)
