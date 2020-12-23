7bea3243926a8bd980337ebdb02936cd2b7209e9
Catalina_out <18395614477@163.com>
2020-12-23 18:08:47 +0800

    update cacheimage

--- migrate/cacheimage.py
+++ migrate/cacheimage.py
@@ -2,6 +2,7 @@
 # /usr/bin/env python
 import os
 import hashlib
+import sys
 from oslo_concurrency import processutils
 from oslo_utils import imageutils
 
@@ -9,50 +10,52 @@ path_base = "/os_instance/_base/"
 
 
 # /os_instance/_base directory is existence
-def is_base(path_base):
-    re_path = os.path.exists(path_base)
-    if re_path is False:
-        try:
-            os.mkdir(path_base)
-        except Exception as exp:
-            print("create directory err: %s" % exp)
-        else:
-            cmd = 'chown nova:nova /os_instance/_base'
-            os.system(cmd)
-
-
-# scp the source instance file to destination compute
-def cory_instance_file(source_ip, instance_id,):
-    instance_file = ['console.log', 'disk.config', 'disk.info']
-    path = '/os_instance/%s'%instance_id
+def is_base(path):
     re_path = os.path.exists(path)
     if re_path is False:
         try:
-            os.mkdir(path_base)
+            os.mkdir(path)
         except Exception as exp:
             print("create directory err: %s" % exp)
         else:
-            cmd = ('chown', 'nova:nova', path)
-            os.system(cmd)
-    for i in instance_file:
-        cmd = ('sshpass', '-p', 'virtmig', 'scp', 'virtmig@', source_ip, ':',instance_id, i, ' ',
-               '/os_instance/', instance_id)
-        try:
-            os.system(cmd)
-        except Exception as exp:
-            print("scp error %s"%exp)
+            cmd = ['chown', 'nova:nova', path]
+	    processutils.execute(*cmd)
 
+
+# scp the source instance file to destination compute
+def copy_instance_file(source_ip, instance_id ):
+    instance_file = ['console.log', 'disk.config', 'disk.info']
+    path = '/os_instance/%s/' % instance_id
+    path_list = list()
+    path_list.append(path)
+    uuid = list()
+    uuid.append(str(path))
+    scp_path='virtmig@%s:%s'%(source_ip,path )
+    re_path = os.path.exists(path)
+    if re_path is False:
+        try:
+            os.mkdir(path)
+        except Exception as exp:
+            print("create directory err: %s" % exp)
+        cmd = ['chown', 'nova:nova'] + path_list
+        processutils.execute(*cmd)
+    for i in instance_file:
+        cmd = ['sshpass', '-p', 'virtmig', 'scp', scp_path+i,] + uuid
+        try:
+            processutils.execute(*cmd)
+        except Exception as exp:
+            print("scp error %s" % exp)
 
 
 # copy instance disk to destination path
 def create_cow_image(instance_id, image_id, dest_path="/os_instance/_base/", size=None):
     base_image_name = hashlib.sha1(image_id.encode('utf-8')).hexdigest()
     base_cmd = ['qemu-img', 'create', '-f', 'qcow2']
-    path="/os_instance/"+instance_id+"/disk"
+    path = "/os_instance/%s/disk" % instance_id 
     cow_opts = []
-    backing_file=dest_path+base_image_name
+    backing_file = dest_path + base_image_name
     cow_opts += ['backing_file=%s' % backing_file]
-    base_details = qemu_img_info(backing_file)
+    base_details = qemu_img_info(dest_path, base_image_name)
     if base_details and base_details.cluster_size is not None:
         cow_opts += ['cluster_size=%s' % base_details.cluster_size]
     if size is not None:
@@ -62,14 +65,16 @@ def create_cow_image(instance_id, image_id, dest_path="/os_instance/_base/", siz
         csv_opts = ",".join(cow_opts)
         cow_opts = ['-o', csv_opts]
     cmd = base_cmd + cow_opts + [path]
+    print cmd
     processutils.execute(*cmd)
+
+
 # qemu-img info the image
 def qemu_img_info(path, image_id):
-    is_base(path_base)
     try:
-        cmd = ('env', 'LC_ALL=C', 'LANG=C', 'qemu-img', 'info', path, image_id)
-        out, err = processutils.execute(cmd)
-    except processutils.ProcessExecutionError as exp:
+        cmd = ('env', 'LC_ALL=C', 'LANG=C', 'qemu-img', 'info', path+ image_id)
+        out, err = processutils.execute(*cmd)
+    except Exception as exp:
         # this means we hit prlimits, make the exception more specific
         if exp.exit_code == -9:
             msg = (("qemu-img aborted by prlimits when inspecting "
@@ -89,20 +94,21 @@ def qemu_img_info(path, image_id):
 
 # convert qcow2 to raw
 def fetch_to_raw(cluster_vip, image_id):
-    is_base()
-    image_path = '/os_gluster_glance/glance/images/'
-    cmd = ('sshpass', '-p', 'virtmig', 'scp', 'virtmig@', cluster_vip, image_path, image_id, path_base)
+    global path_base
+    is_base(path_base)
+    image_tmp = '/os_gluster_glance/glance/images/%s'%image_id
+    scp_path='virtmig@%s:%s'%(cluster_vip,image_tmp)
+    cmd = ['sshpass', '-p', 'virtmig', 'scp', scp_path, path_base] 
     try:
-        os.system(cmd)
+	processutils.execute(*cmd)
     except Exception as exp:
         print("the transfer is failed:%s" % exp)
     # hashlib the qcow2
     base_image_name = hashlib.sha1(image_id.encode('utf-8')).hexdigest()
-    os.chdir(path_base)
-    os.rename(image_id, base_image_name)
-    cmd = ('qemu-img', 'convert', '-f', 'qcow2', '-O', 'raw', image_id, base_image_name)
+    #os.rename(path_base+image_id, path_base+image_name)
+    cmd = ('qemu-img', 'convert', '-f', 'qcow2', '-O', 'raw', path_base+image_id, path_base+base_image_name)
     try:
-        processutils.execute(cmd)
+        processutils.execute(*cmd)
     except processutils.ProcessExecutionError as exp:
         msg = (("Unable to convert image to %(format)s: %(exp)s") % {'format': image_id, 'exp': exp})
         print(msg)
@@ -110,3 +116,13 @@ def fetch_to_raw(cluster_vip, image_id):
     if data.file_format != "raw":
         raise (("Converted to raw, but format is now %s") % data.file_format)
 
+
+def main(argv):
+    #copy_instance_file(argv[1],argv[2])
+    #fetch_to_raw(argv[3], argv[4])
+    create_cow_image(argv[2], argv[4])
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
