2101cd5081f0cb4821e8b2c28bcce9be301fac77
Catalina_out <18395614477@163.com>
2020-12-24 10:47:49 +0800

    add  create_iscsi_target

--- migrate/createlvm.py
+++ migrate/createlvm.py
@@ -5,7 +5,7 @@ import os
 import sys
 from oslo_concurrency import processutils
 
-
+# create lvm
 def create_volume(volume_id, size_str):
     vg_name = 'vg_os'
     volume_name = 'volume-%s' % volume_id
@@ -14,25 +14,7 @@ def create_volume(volume_id, size_str):
         processutils.execute(*cmd, run_as_root=True)
     except Exception as exp:
         print  exp
-
-
-def initialize_connection(volume_id, auth_user, auth_pass):
-    volume_iqn = 'iqn.2010-10.org.openstack'
-    volume_iqn = volume_iqn + ":" + 'volume-%s' % volume_id
-    name = os.popen("cat /etc/iscsi/initiatorname.iscsi |awk -F'=' '{print $2}'")
-    target_name = name.read()
-    name.close()
-    try:
-        processutils.execute('cinder-rtstool', 'add-initiator',
-                             volume_iqn,
-                             auth_user,
-                             auth_pass,
-                             target_name,
-                             run_as_root=True)
-    except Exception as exp:
-        print exp
-
-
+# create iqn
 def create_iscsi_target(volume_id, ip, user="virtmig", password="virtmig", port=3260):
     lv_name = '/dev/vg_os/volume-%s' % volume_id
     target_name = 'iqn.2010-10.org.openstack' + ":" + 'volume-%s' % volume_id
@@ -50,10 +32,29 @@ def create_iscsi_target(volume_id, ip, user="virtmig", password="virtmig", port=
     except Exception as err:
         print err
 
+# initialize iqn
+def initialize_connection(volume_id, auth_user, auth_pass):
+    volume_iqn = 'iqn.2010-10.org.openstack'
+    volume_iqn = volume_iqn + ":" + 'volume-%s' % volume_id
+    name = os.popen("cat /etc/iscsi/initiatorname.iscsi |awk -F'=' '{print $2}'")
+    target_name = name.read()
+    name.close()
+    try:
+        processutils.execute('cinder-rtstool', 'add-initiator',
+                             volume_iqn,
+                             auth_user,
+                             auth_pass,
+                             target_name,
+                             run_as_root=True)
+    except Exception as exp:
+        print exp
+
+
 
 def main(argv):
-    create_volume(argv[1], argv[2])
-    initialize_connection(argv[1], argv[3], argv[4])
+    create_volume(argv[1], argv[3])
+    create_iscsi_target(argv[1], argv[2])
+    initialize_connection(argv[1], argv[4], argv[4])
     pass
 
 
