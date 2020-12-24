4ff39414ec671d4d348afb646bcd42f973c8ad1e
Catalina_out <18395614477@163.com>
2020-12-24 10:40:49 +0800

    update lvmcreate

--- migrate/createlvm.py
+++ migrate/createlvm.py
@@ -5,6 +5,7 @@ import os
 import sys
 from oslo_concurrency import processutils
 
+
 def create_volume(volume_id, size_str):
     vg_name = 'vg_os'
     volume_name = 'volume-%s' % volume_id
@@ -31,11 +32,30 @@ def initialize_connection(volume_id, auth_user, auth_pass):
     except Exception as exp:
         print exp
 
+
+def create_iscsi_target(volume_id, ip, user="virtmig", password="virtmig", port=3260):
+    lv_name = '/dev/vg_os/volume-%s' % volume_id
+    target_name = 'iqn.2010-10.org.openstack' + ":" + 'volume-%s' % volume_id
+    try:
+        optional_args = []
+        optional_args.append('-p%s' % port)
+        optional_args.append('-a%s' % ip)
+        command_args = ['cinder-rtstool',
+                        'create',
+                        lv_name,
+                        target_name,
+                        user,
+                        password, 'False', ] + optional_args
+        processutils.execute(*command_args, run_as_root=True)
+    except Exception as err:
+        print err
+
+
 def main(argv):
-    lis = list()
-    lis.append(argv[1])
+    create_volume(argv[1], argv[2])
+    initialize_connection(argv[1], argv[3], argv[4])
     pass
+
 
 if __name__ == '__main__':
     main(sys.argv)
-
